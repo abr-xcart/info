cfca127954c91a594f47dea1d8fef3cf3cc1bd8f
Yuriy Abramov <abr@x-cart.com>
2018-05-16 18:20:26 +0400

    backup:clone

--- backup.sh
+++ backup.sh
@@ -10,5 +10,9 @@ if [ ! -d /opt/cdev/backup/backup ]; then
 	git clone git@bitbucket.org:kas_p/backup.git
 	cd /opt/cdev/backup/backup
 	git checkout DEDIC
+else
+	cd /opt/cdev/backup/backup
+	git fetch
+	git branch
 fi
 /opt/cdev/backup/backup/setup.sh

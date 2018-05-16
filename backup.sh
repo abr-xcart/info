8bd06258f0ace81e88734794280ee76eb0473ee1
Yuriy Abramov <abr@x-cart.com>
2018-05-16 18:19:39 +0400

    backup:clone

--- backup.sh
+++ backup.sh
@@ -6,7 +6,9 @@ pip install --upgrade setuptools
 pip install urllib3 --force --upgrade
 pip install pyxchl
 cd /opt/cdev/backup
-git clone git@bitbucket.org:kas_p/backup.git
-cd /opt/cdev/backup/backup
-git checkout DEDIC
+if [ ! -d /opt/cdev/backup/backup ]; then
+	git clone git@bitbucket.org:kas_p/backup.git
+	cd /opt/cdev/backup/backup
+	git checkout DEDIC
+fi
 /opt/cdev/backup/backup/setup.sh

2424fd344142cc6e56a939b8f7f4a57e1a1c886e
Yuriy Abramov <YAbramov@ibs.ru>
2025-03-15 18:42:51 +0400

    x-dev-remote-sertup.sh

--- x-dev-remote-sertup.sh
+++ x-dev-remote-sertup.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+# Создаем временный файл
+temp_file=$(mktemp)
+
+# Устанавливаем ловушку для автоматического удаления временного файла при завершении скрипта
+trap 'rm -f "$temp_file"' EXIT
+
 # Скачиваем удаленный файл во временный файл
-wget -O ~/.bash_aliases.x-dev https://info.x-dev.us/x-dev-remote-sertup/bash_aliases
+if ! wget -O "$temp_file" https://info.x-dev.us/x-dev-remote-sertup/bash_aliases; then
+    echo "Ошибка: не удалось скачать файл." >&2
+    exit 1
+fi
 
 # Проверяем, существует ли оригинальный файл
 if [ -e ~/.bash_aliases ]; then
     # Сравниваем содержимое файлов
-    if ! cmp -s ~/.bash_aliases ~/.bash_aliases.x-dev; then
+    if ! cmp -s ~/.bash_aliases "$temp_file"; then
         # Если файлы различаются, открываем vimdiff
-        vimdiff ~/.bash_aliases ~/.bash_aliases.x-dev
+        vimdiff ~/.bash_aliases "$temp_file"
     else
-        # Если файлы идентичны, удаляем временный файл
-        rm ~/.bash_aliases.x-dev
-        echo "Файлы идентичны. Временный файл удален."
+        # Если файлы идентичны, выводим сообщение и завершаем скрипт
+        echo "Файлы идентичны. Временный файл будет удален."
         exit 0
     fi
 else
-    # Если оригинальный файл не существует, просто перемещаем временный файл
-    mv ~/.bash_aliases.x-dev ~/.bash_aliases
+    # Если оригинальный файл не существует, перемещаем временный файл на его место
+    mv "$temp_file" ~/.bash_aliases
     echo "Файл ~/.bash_aliases создан."
 fi
